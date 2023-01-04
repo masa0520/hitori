bf604d062737dc22cff5e948338eff836a50be64
masa0520 <masahiro130520@icloud.com>
2023-01-04 16:51:19 +0900

    [add] ホームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
