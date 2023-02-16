e184c766f32ef7a0d3c0420970aa5b937a9a4e9e
masa0520 <masahiro130520@icloud.com>
2023-02-17 01:02:10 +0900

    [fix] before_actionを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,10 @@
 class LikesController < ApplicationController
+  before_action :set_store
+
   def create
-    @store = Store.find(params[:store_id])
     @liked = Like.new(user_id: current_user.id, store_id: params[:store_id])
     @liked.save
+
     render turbo_stream: turbo_stream.replace(
       'like',
       partial: 'likes/like',
@@ -11,13 +13,19 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @store = Store.find(params[:store_id])
     @liked = Like.find_by(user_id: current_user.id, store_id: params[:store_id])
     @liked.destroy!
+
     render turbo_stream: turbo_stream.replace(
       'like',
       partial: 'likes/like',
       locals: { store: @store },
     )
+  end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:store_id])
   end
 end
