2e0727f7f6739b8f07d783e1da28748d13c333eb
masa0520 <masahiro130520@icloud.com>
2023-02-17 01:00:17 +0900

    [fix] bookmark機能を非同期

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,31 @@
 class BookmarksController < ApplicationController
+  before_action :set_store
+
   def create
     @bookmark =  Bookmark.new(user_id: current_user.id, store_id: params[:store_id])
     @bookmark.save
-    redirect_back fallback_location: stores_path
+    
+    render turbo_stream: turbo_stream.replace(
+      'bookmark',
+      partial: 'bookmarks/bookmark',
+      locals: { store: @store },
+    )
   end
 
   def destroy
     @bookmark = Bookmark.find_by(user_id: current_user.id, store_id: params[:store_id])
     @bookmark.destroy!
-    redirect_back fallback_location: stores_path
+    
+    render turbo_stream: turbo_stream.replace(
+      'bookmark',
+      partial: 'bookmarks/bookmark',
+      locals: { store: @store },
+    )
+  end
+
+  private
+
+  def set_store
+    @store = Store.find(params[:store_id])
   end
 end
